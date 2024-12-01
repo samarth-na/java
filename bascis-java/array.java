c7545783b1869b4e820da3a24527bbe84966ba9b
Samarth Nagar <samarthxma2009@gmail.com>
2024-12-01 22:29:49 +0530

    main backup

--- bascis-java/array.java
+++ bascis-java/array.java
@@ -3,6 +3,7 @@ class array {
         int[] numbers = { 1, 2, 3, 4, 5, 6 };
         int[] positiveNumbers = numbers; // copying arrays
 
+
         for (int number : positiveNumbers) {
             System.out.print(number + ", ");
         }
